0c03f61e6e4ca3db852c46be358049e21b56ae65
Samantha Wakefield <sam@housenhome.co.uk>
2017-01-17 17:21:16 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,57 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour =  "king";
+
+
+/* if(cardTwo === cardFour){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   if(cardTwo === cardThree){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   if(cardTwo === cardOne){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   if(cardOne=== cardFour){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   if(cardOne=== cardThree){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   if(cardThree=== cardFour){
+   alert('You found a match!')
+   }
+else{
+   alert('Sorry, try again.')
+   }
+   */
+   
+var createCards = function(){
+		var Board = document.getElementById('game-board');
+   		for (var i=0; i<=3; i++) {
+
+   		/* create an element with class = card */		
+   		var card = document.createElement('div');
+   		    card, className = 'card';
+   		}
+   		
+   		/*append new card to the game-board*/
+   		card.appendChild(Board);
+
+    createCards();
+   }
