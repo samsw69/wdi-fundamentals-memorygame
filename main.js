29a78f8eeb04f9bd898d5c695afa4014185b49ab
Samantha Wakefield <sam@housenhome.co.uk>
2017-01-18 19:32:38 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour =  "king";
+var cardFour = "king";
+
+if (cardOne === cardFour),  {
+alert("you found a match");
+}
+else if (cardOne === cardThree) {
+alert("you found a match");
+}
+else if (cardOne === cardTwo) {
+alert("you found a match");
+}
+else if (cardTwo === cardFour) {
+alert("you found a match");
+}
+else if (cardTwo === cardThree) {
+alert("you found a match");
+}
+else if (cardThree === cardFour) {
+alert("you found a match");
+}
+else alert("no match!");
+	
